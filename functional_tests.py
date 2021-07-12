0302ad59f56d5cfe465fcf31d1ea07ba9a7ef6b7
Kris Litman <kris.d.litman@gmail.com>
2021-07-11 22:08:21 -0400

    Added first end to end test with unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import unittest
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get('http://localhost:8000')
+class UserVisitsPageTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_get_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('Habits', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
