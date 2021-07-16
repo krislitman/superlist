479ee3f0e7e4048811ef15d92b3ff6aff27a05f7
Kris Litman <kris.d.litman@gmail.com>
2021-07-16 13:44:33 -0400

    Built out first unittest

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_stuff(self):
-        self.assertEqual(1 + 1, 100)
+    def test_root_url_resolves_to_home_page_view(self):
+        path = resolve('/')
+        self.assertEqual(path.func, home)
