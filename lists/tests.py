e3bf850be7abd16edc406b1f605426fda58dd0e8
Kris Litman <kris.d.litman@gmail.com>
2021-08-06 22:12:41 -0400

    Added item test class to tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -25,3 +26,22 @@ class HomePageTest(TestCase):
         self.assertTrue(content.strip().endswith('</html>'))
 
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTestCase(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first = Item()
+        first.text = 'This is the first item'
+        first.save()
+
+        second = Item()
+        second.text = 'This is the second item'
+        second.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'This is the first item')
+        self.assertEqual(second_saved_item.text, 'This is the second item')
