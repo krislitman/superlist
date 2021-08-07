75b55df799ea826f7bb29488de16546ced93004f
Kris Litman <kris.d.litman@gmail.com>
2021-08-06 22:32:28 -0400

    Added an item to the home view

--- lists/tests.py
+++ lists/tests.py
@@ -45,3 +45,13 @@ class ItemModelTestCase(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'This is the first item')
         self.assertEqual(second_saved_item.text, 'This is the second item')
+
+    def test_can_save_a_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
