d841eb5cee9e72fe2977e4251c481efc24301d32
Kris Litman <kris.d.litman@gmail.com>
2021-08-07 20:57:38 -0400

    On to chapter 7

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 import time
+
+MAX_WAIT = 10
 
 
 class UserVisitsPageTest(LiveServerTestCase):
@@ -40,9 +43,7 @@ class UserVisitsPageTest(LiveServerTestCase):
 
         # ? When user hits enter, the page updates, and now the page lists
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table('1: Buy a new computer')
+        self.wait_for_row_in_list_table('1: Buy a new computer')
 
         # ? "1: Buy a new computer" as an item in a to-do list
         table = self.browser.find_element_by_id('id_list_table')
@@ -51,3 +52,16 @@ class UserVisitsPageTest(LiveServerTestCase):
 
         # ? There is still a text box inviting user to add another item.
         self.fail('Test isnt done')
+
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
